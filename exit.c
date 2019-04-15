31775813b5d6b49548070142d3c0a4ae98d11f8e
macoyulloa <47118702+macoyulloa@users.noreply.github.com>
2019-04-15 16:52:34 -0500

    Update exit.c

--- exit.c
+++ exit.c
@@ -13,11 +13,5 @@ int exit_built_in(char **arguments)
                free(arguments);
                _exit(0);
        }
-       else
-       {
-               status = _atoi(arguments[1]);
-               free(arguments);
-               _exit(status);
-       }
        return (1);
 }
