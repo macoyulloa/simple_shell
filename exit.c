5e53717dc9f636731bb5adb295649b5a73e76a5b
Diegokernel <47120841+Diegokernel@users.noreply.github.com>
2019-04-15 16:16:37 -0500

    Update exit.c

--- exit.c
+++ exit.c
@@ -1,12 +1,23 @@
 #include "shell.h"
 /**
- * exit_built_in - exit the program.
- * @arguments: the arguments that are passing.
- * Return: 1.
- */
-void exit_built_in(char **arguments)
+* exit_built_in - exit the program.
+* @arguments: the arguments that are passing.
+* Return: 1.
+*/
+int exit_built_in(char **arguments)
 {
-	free(arguments);
-	exit(1);
-}
+       int status;
 
+       if (!arguments[1])
+       {
+               free(arguments);
+               _exit(0);
+       }
+       else
+       {
+               status = _atoi(arguments[1]);
+               free(arguments);
+               _exit(status);
+       }
+       return (1);
+}
