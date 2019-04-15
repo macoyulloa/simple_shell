3d69366608f9d4131161c7e33d30589898532537
macoyulloa <47118702+macoyulloa@users.noreply.github.com>
2019-04-15 12:57:12 -0500

    Create _strdup.c

---
+++ _strdup.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+/**
+ * _strdup - duplicate the string str using the head memory, wirth malloc.
+ * @str: string to be duplicate.
+ * Return: a pointer to a new string which is a duplicate of str.
+ */
+char *_strdup(char *str)
+{
+	char *str1;
+	int leng;
+
+	str1 = str;
+	if (str != NULL)
+	{
+		for (leng = 0; str1[leng] != '\0'; leng++)
+		{
+		}
+		leng = leng + 1;
+		str1 = malloc(sizeof(char) * (leng + 1));
+		if (str1 == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (leng = 0; str[leng] != '\0'; leng++)
+				str1[leng] = str[leng];
+			return (str1);
+		}
+	}
+	return (NULL);
+}
