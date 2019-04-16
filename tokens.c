40169d9fd400c1ed2b0e4ec7ac3e23a2656e0e31
Diegokernel <47120841+Diegokernel@users.noreply.github.com>
2019-04-15 19:22:38 -0500

    Update tokens.c

--- tokens.c
+++ tokens.c
@@ -14,7 +14,7 @@ char **gettoken(char *string)
 	arguments = malloc(sizeof(char *) * size);
 	if (!arguments)
 	{
-		freed(arguments);
+		free(arguments);
 		exit(1);
 	}
 	arguments[i] = _strtok(string, "\n ");
